4e410304fb70d5b0641e09b849339acefacc06fe
Rickard Garnau <rickard.garnau89@gmail.com>
2026-02-18 09:47:51 +0100

    commit latest changes

--- flagged_data.py
+++ flagged_data.py
@@ -1,46 +1,38 @@
-from string import whitespace
-
 import pandas as pd
-from websockets.cli import print_during_input
 
-df = pd.read_csv("products.csv", sep=";")
-
+df = pd.read_csv("products_cleaned.csv")
 branching_df = df.copy()
 
-missing_df["id"] = missing_df["id"].isna()
-missing_df["name"] = missing_df["name"].isna()
-missing_df["price"] = missing_df["price"].isna()
-missing_df["currency"] = missing_df["currency"].isna()
-missing_df["created_at"] = missing_df["created_at"].isna()
-
-
+# --- CONDITIONS ---
 missing_id_condition = branching_df["id"].isna()
-df_missing_ids = branching_df[missing_id_condition].copy()
-df_missing_ids.to_csv("missing_ids.csv", index=False)
-
 wrong_name_condition = (branching_df["name"].astype(str).str.strip() == "") & branching_df["name"].notna()
-df_wrong_name = branching_df[wrong_name_condition].copy()
-df_wrong_name.to_csv("invalid_names.csv", index=False)
-
-wrong_price_condition = pd.to_numeric(branching_df["price"], errors='coerce').isna() & branching_df["price"].notna()
-df_wrong_price = branching_df[wrong_price_condition].copy()
-df_wrong_price.to_csv("invalid_prices.csv", index=False)
+invalid_price_condition = pd.to_numeric(branching_df["price"], errors='coerce').isna()
+wrong_price_condition = (
+    (branching_df["price"] < 0) |
+    (branching_df["price"] == 0) |
+    (branching_df["price"] > 4000)
+) & branching_df["price"].notna()
 
 wrong_currency_condition = (branching_df["currency"].astype(str).str.strip().str.len() != 3) & branching_df["currency"].notna()
-df_wrong_currency = branching_df[wrong_currency_condition].copy()
-df_wrong_currency.to_csv("invalid_currency.csv", index=False)
-
 wrong_date_condition = pd.to_datetime(branching_df["created_at"], errors='coerce').isna() & branching_df["created_at"].notna()
-df_wrong_date = branching_df[wrong_date_condition].copy()
-df_wrong_date.to_csv("invalid_dates.csv", index=False)
 
-
+# --- AVVISA ---
 branching_df["is_rejected"] = (
     missing_id_condition |
+    invalid_price_condition  # text istället för siffra
+)
+
+# --- FLAGGA ---
+branching_df["is_flagged"] = (
     wrong_name_condition |
-    wrong_price_condition |
+    wrong_price_condition |  # 0, negativ, extremt hög
     wrong_currency_condition |
     wrong_date_condition
 )
 
-
+# --- SPARA ---
+rejected_df = branching_df[branching_df["is_rejected"] == True]
+rejected_df.to_csv("rejected_values.csv", index=False)
+flagged_df = branching_df[branching_df["is_flagged"] == True]
+flagged_df.to_csv("flagged_values.csv", index=False)
+branching_df.to_csv("products_flagged.csv", index=False)
